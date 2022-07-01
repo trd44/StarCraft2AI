346d4aeab6c85ff7136efb7912613c7f0923ff37
trd44 <trd44@cornell.edu>
2022-07-01 14:19:13 -0700

    Committing before removing anaconda

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,25 @@
+from operator import ne
+from turtle import up
+import sc2
 from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.upgrade_id import UpgradeId
+
+# pip3 install gym[box2d]
+import gym
+env = gym.make("LunarLander-v2")
+env.reset()
+
+print("sample action:", env.action_space.sample())
 
 
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
     """This bot's name"""
 
-    RACE: Race = Race.Terran
+    RACE: Race = Race.Protoss
     """This bot's Starcraft 2 race.
     Options are:
         Race.Terran
@@ -27,7 +40,120 @@ class CompetitiveBot(BotAI):
         This code runs continually throughout the game
         Populate this function with whatever your bot should do!
         """
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_gateway()
+        await self.build_gas()
+        await self.build_cyber_core()
+        await self.train_stalkers()
+        await self.build_four_gates()
+        await self.chrono()
+        await self.warpgate_research()
+        await self.attack()
+        
         pass
+
+    async def build_workers(self):
+        nexus = self.townhalls.ready.random
+        if(
+            self.can_afford(UnitTypeId.PROBE)
+            and nexus.is_idle
+            and self.workers.amount < self.townhalls.amount * 22
+        ):
+            nexus.train(UnitTypeId.PROBE)
+
+    async def build_pylons(self):
+        nexus = self.townhalls.ready.random
+        pos = nexus.position.towards(self.enemy_start_locations[0],10)
+
+        if(
+            self.supply_left < 3 and self.already_pending(UnitTypeId.PYLON) == 0
+            and self.can_afford(UnitTypeId.PYLON)
+        ):
+            await self.build(UnitTypeId.PYLON, near = pos)
+    
+    async def build_gateway(self):
+        if(
+            self.structures(UnitTypeId.PYLON).ready
+            and self.can_afford(UnitTypeId.GATEWAY)
+            and not self.structures(UnitTypeId.GATEWAY)
+        ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+
+    async def build_gas(self):
+        if self.structures(UnitTypeId.GATEWAY):
+            for nexus in self.townhalls.ready:
+                vgs = self.vespene_geyser.closer_than(15, nexus)
+                for vg in vgs:
+                    if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                        break
+                    worker = self.select_build_worker(vg.position)
+                    if worker is None:
+                        break
+                    if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                        worker.build(UnitTypeId.ASSIMILATOR, vg)
+                        worker.stop(queue=True)
+
+    async def build_cyber_core(self):
+        if self.structures(UnitTypeId.PYLON).ready:
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            if self.structures(UnitTypeId.GATEWAY).ready:
+                if not self.structures(UnitTypeId.CYBERNETICSCORE):
+                    if(
+                        self.can_afford(UnitTypeId.CYBERNETICSCORE)
+                        and self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0
+                    ):
+                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon)        
+
+    async def train_stalkers(self):
+        for gateway in self.structures(UnitTypeId.GATEWAY).ready:
+            if(
+                self.can_afford(UnitTypeId.STALKER)
+                and gateway.is_idle
+            ):
+                gateway.train(UnitTypeId.STALKER)
+
+    async def build_four_gates(self):
+        if (
+            self.structures(UnitTypeId.PYLON).ready
+            and self.can_afford(UnitTypeId.GATEWAY)
+            and self.structures(UnitTypeId.GATEWAY).amount < 4
+        ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+        
+    async def chrono(self):
+        if self.structures(UnitTypeId.PYLON):
+            nexus = self.townhalls.ready.random
+            if (
+                not self.structures(UnitTypeId.CYBERNETICSCORE).ready
+                and self.structures(UnitTypeId.PYLON).amount > 0
+            ):
+                if nexus.energy >= 50:
+                    nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus)
+            else:
+                if nexus.energy >= 50:
+                    cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.first
+                    nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, cybercore)
+
+    async def warpgate_research(self):
+        if(
+            self.structures(UnitTypeId.CYBERNETICSCORE).ready
+            and self.can_afford(AbilityId.RESEARCH_WARPGATE)
+            and self.already_pending_upgrade(UpgradeId.WARPGATERESEARCH)
+        ):
+            cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.first
+            cybercore.research(UpgradeId.WARPGATERESEARCH)
+
+    async def attack(self):
+        stalkercount = self.units(UnitTypeId.STALKER).amount
+        stalkers = self.units(UnitTypeId.STALKER)
+
+        for stalker in stalkers:
+            if stalkercount > 4:
+                stalker.attack(self.enemy_start_locations[0])
 
     async def on_end(self, result: Result):
         """
